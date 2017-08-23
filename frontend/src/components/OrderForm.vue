72cfff276b4ab19ace2e2636345a03285224b04b
Buglight <buglight@kistriver.com>
2017-08-23 16:14:55 +0300

    Data bindings in form

--- frontend/src/components/OrderForm.vue
+++ frontend/src/components/OrderForm.vue
@@ -4,50 +4,50 @@
             <div class='form-group'>
                 <label class='col-md-4 control-label'>Название фирмы отправителя</label>
                 <div class='col-md-8'>
-                    <input type='text' class='form-control'>
+                    <input v-model='form.companyName' type='text' class='form-control'>
                 </div>
             </div>
             <div class='form-group'>
                 <label class='col-md-4 control-label'>Город погрузки</label>
                 <div class='col-md-8'>
-                    <CityInput></CityInput>
+                    <CityInput v-model='form.loadCity'></CityInput>
                 </div>
             </div>
             <div class='form-group'>
                 <label class='col-md-4 control-label'>Город назначения</label>
                 <div class='col-md-8'>
-                    <CityInput></CityInput>
+                    <CityInput v-model='form.endCity'></CityInput>
                 </div>
             </div>
             <div class='form-group'>
                 <label class='col-md-4 control-label'>ФИО заказчика</label>
                 <div class='col-md-8'>
-                    <input type='text' class='form-control'>
+                    <input v-model='form.clientFullName' type='text' class='form-control'>
                 </div>
             </div>
             <div class='form-group'>
                 <label class='col-md-4 control-label'>Телефон заказчика</label>
                 <div class='col-md-8'>
-                    <input type='tel' class='form-control'>
+                    <input v-model='form.clientPhone' type='tel' class='form-control'>
                 </div>
             </div>
             <div class='form-group'>
                 <label class='col-md-4 control-label'>Email заказчика</label>
                 <div class='col-md-8'>
-                    <input type='email' class='form-control'>
+                    <input v-model='form.clientMail' type='email' class='form-control'>
                 </div>
             </div>
             <div class='form-group'>
                 <label class='col-md-4 control-label'>Тип груза</label>
                 <div class='col-md-8'>
-                    <input type='text' class='form-control'>
+                    <input v-model='form.cargoType' type='text' class='form-control'>
                 </div>
             </div>
             <div class='form-group'>
                 <label class='col-md-4 control-label'>Вес груза</label>
                 <div class='col-md-8'>
                     <div class='input-group'>
-                        <input type='number' class='form-control'>
+                        <input v-model='form.cargoWeight' type='number' class='form-control'>
                         <div class='input-group-addon'>кг</div>
                     </div>
                 </div>
@@ -56,7 +56,7 @@
                 <label class='col-md-4 control-label'>Объем груза</label>
                 <div class='col-md-8'>
                     <div class='input-group'>
-                        <input type='number' class='form-control'>
+                        <input v-model='form.cargoVolume' type='number' class='form-control'>
                         <div class='input-group-addon'>м<sup>3</sup></div>
                     </div>
                 </div>
@@ -64,7 +64,7 @@
             <div class='form-group'>
                 <label class='col-md-4 control-label'>Дата погрузки</label>
                 <div class='col-md-8'>
-                    <input type='date' class='form-control'>
+                    <input v-model='form.loadDate' type='date' class='form-control'>
                 </div>
             </div>
             <div class='form-group'>
@@ -82,6 +82,22 @@
 import CityInput from './CityInput.vue';
 
 export default {
+    data() {
+        return {
+            form: {
+                companyName: '',
+                loadCity: '',
+                endCity: '',
+                clientFullName: '',
+                clientPhone: '',
+                clientMail: '',
+                cargoType: '',
+                cargoWeight: '',
+                cargoVolume: '',
+                loadDate: ''
+            }
+        };
+    },
     components: {
         CityInput
     }
