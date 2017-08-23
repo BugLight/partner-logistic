157c6d0d12f753e03ec8042a651edb4b4b66c257
Buglight <buglight@kistriver.com>
2017-08-23 23:53:15 +0300

    Unfolding navbar on mobiles

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -3,18 +3,26 @@
         <nav class='navbar navbar-default navbar-fixed-top' role='navigation'>
             <div class='container-fluid'>
                 <div class='navbar-header'>
+                    <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar-collapse' aria-expanded='false'>
+                        <span class='sr-only'>Toggle navigation</span>
+                        <span class='icon-bar'></span>
+                        <span class='icon-bar'></span>
+                        <span class='icon-bar'></span>
+                    </button>
                     <a @click='makeActive()' class='navbar-brand' href='#'>Партнер Логистик</a>
                 </div>
-                <ul class='nav navbar-nav navbar-right'>
-                    <NavLink
-                    v-for="link in links"
-                    @click.native='makeActive(link.name)'
-                    :href='link.url'
-                    :active='link.name == active'
-                    :key='link.name'>
-                        {{link.text}}
-                    </NavLink>
-                </ul>
+                <div class='collapse navbar-collapse' id='navbar-collapse'>
+                    <ul class='nav navbar-nav navbar-right'>
+                        <NavLink
+                        v-for='link in links'
+                        @click.native='makeActive(link.name)'
+                        :href='link.url'
+                        :active='link.name == active'
+                        :key='link.name'>
+                            {{link.text}}
+                        </NavLink>
+                    </ul>
+                </div>
             </div>
         </nav>
     </header>
